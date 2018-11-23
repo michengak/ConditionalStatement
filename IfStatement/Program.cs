5e8a7279da61dc0c3660d73bb82820906383f26a
michel ngako <emoumichel@gmail.com>
2018-11-23 15:41:12 -0500

    Using switch to replace if statement

--- IfStatement/Program.cs
+++ IfStatement/Program.cs
@@ -11,7 +11,7 @@ namespace IfStatement
         static void Main(string[] args)
         {
 
-                                  // raw use of if satement with the use of && / || percent 
+            // raw use of if satement with the use of && / || percent 
 
             //Console.WriteLine("Please enter a number");
 
@@ -38,33 +38,60 @@ namespace IfStatement
 
             //}
 
-                                             //Better use version of if statement
+            //Better use version of if statement
 
+
+            //Console.WriteLine("Please enter a number");
+
+            //int UserNumber = int.Parse(Console.ReadLine());
+
+            //if (UserNumber == 1)
+            //{
+            //    Console.WriteLine("Your number is one");
+
+            //}
+            //else if (UserNumber == 2)
+            //{
+            //    Console.WriteLine("Your number is two");
+
+            //}
+            //else if (UserNumber == 3)
+            //{
+            //    Console.WriteLine("Your number is three");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Your number is not between 1 and 3");
+
+            //}
+
+
+
+            // rewrite using Switch statement
 
             Console.WriteLine("Please enter a number");
+            int UserNumer = int.Parse(Console.ReadLine());
 
-            int UserNumber = int.Parse(Console.ReadLine());
-
-            if (UserNumber == 1)
+            switch (UserNumer)
             {
-                Console.WriteLine("Your number is one");
+                case 10:
+                    Console.WriteLine("Your number is 10");
+                    break;
 
-            }
-            else if (UserNumber == 2)
-            {
-                Console.WriteLine("Your number is two");
+                case 20:
+                    Console.WriteLine("Your number is 20");
+                    break;
 
+                case 30:
+                    Console.WriteLine("Your number is 30");
+                    break;
+                default:
+                    Console.WriteLine("Your number is not 10, 20, 30);
+                    break;
             }
-            else if (UserNumber == 3)
-            {
-                Console.WriteLine("Your number is three");
 
-            }
-            else
-            {
-                Console.WriteLine("Your number is not between 1 and 3");
 
-            }
         }
     }
 }
